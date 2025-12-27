74c91f8788ab2f1fccc213428270403922784fda
bmenezes05 <bmenezes05@gmail.com>
2025-12-27 00:20:12 -0300

    project refactor

--- src/scenes/bootScene.js
+++
@@ -1,9 +0,0 @@
-import { AudioManager } from '../audioManager.js';
-
-export class BootScene extends Phaser.Scene {
-    constructor() { super({ key: 'BootScene' }); }
-    create() {
-        this.audio = new AudioManager(this);
-        //this.audio.play('bgm', { loop: true, volume: 0.5 });
-    }
-}
